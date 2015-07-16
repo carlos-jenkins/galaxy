cf049c3544675cd72a51435a905716f787f75be0
Carlos Miguel Jenkins Perez <carlos.jenkins@hp.com>
2015-07-16 15:30:35 -0600

    Changing default to non fullscreen and fixing a bug.

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -7,37 +7,34 @@ Menu::Menu() {
 }
 
 Menu::~Menu() {
-		
-	//Free resources
-	delete buttons;
 }
 
 //Initialize every component of the menu
 void Menu::Init() {
-	
+
 	//Init the buttons of the menu
 	selection = 0;							//Init the selection
-	
+
 	//Specific configuration
 	int x = 374;
 	int y = 284;
-	
+
 	//First button, PLAY
 	buttons[0].x = x;						//Real screen coordenates
 	buttons[0].y = y;
-	
+
 	//First button, SCORE
 	buttons[1].x = x;						//Real screen coordenates
 	buttons[1].y = (y + 100);
-	
+
 	//First button, CREDITS
 	buttons[2].x = x;						//Real screen coordenates
 	buttons[2].y = (y + 200);
-	
+
 	//First button, EXIT
 	buttons[3].x = x;						//Real screen coordenates
 	buttons[3].y = (y + 300);
-	
+
 	//General configuration
 	int h = 46;
 	int w = 276;
@@ -55,19 +52,19 @@ void Menu::Init() {
 	}
 }
 
-//Returns the index of the selected button, -1 if no button is selected 
+//Returns the index of the selected button, -1 if no button is selected
 int Menu::Over(int param_x, int param_y) {
-	
+
 	//Assumes that no button is selected
     int button_selected = -1;
-    
+
     for(int n = 0; n < NUMBER_BUTTONS; n++) {
 		if(buttons[n].Over(param_x, param_y)) {
 			button_selected = n;
 			n = NUMBER_BUTTONS;	//To breack the cycle.
 		}
     }
-	
+
 	return button_selected;
 }
 
@@ -75,10 +72,10 @@ int Menu::Over(int param_x, int param_y) {
 //Return -1 if the selection hasn't change, otherwise return the selection index.
 //Note: 0 = no button selected.
 int Menu::Changed(int param_x, int param_y) {
-    
+
     //Backup the old selection
     int old_selection = selection;
-    
+
     //Assumes that no button is selected
     selection = 0;
     for(int n = 0; n < NUMBER_BUTTONS; n++) {
@@ -87,7 +84,7 @@ int Menu::Changed(int param_x, int param_y) {
 			n = NUMBER_BUTTONS;	//To breack the cycle.
 		}
     }
-    
+
     if(old_selection == selection) {			//The selection hasn't change.
     	return -1;
     } else {
